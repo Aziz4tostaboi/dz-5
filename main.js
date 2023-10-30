514c3a49efd8b2aae4906b877b490978e570e624
Aziz4tostaboi <abylovaziz@gmail.com>
2023-10-30 10:28:02 +0600

    firsit-commit

---
+++ main.js
@@ -0,0 +1,67 @@
+///--start--work--////
+/*Создайте две функции, add и multiply; пусть каждая принимает
+по два аргумента. Функция add должна складывать аргументы
+и возвращать результат, а функция multiply — перемножать
+аргументы.
+    С помощью только этих двух функций вычислите следующее
+несложное выражение: 36325 * 9824 + 777
+ */
+"use strict"
+
+function multiply(a , b){
+    return a * b
+}
+function add( a, b ){
+    return a + b
+}
+
+let res =multiply(36325, 9824)
+console.log(add(res, 777))
+/// Сделайте функцию, которая возвращает квадрат числа. Число передается параметром.///
+//
+// function showSquare(){
+//     let a=prompt("число");
+//     let b=a**2;
+//     prompt(`результат:${b}`)
+// }
+// showSquare()//
+
+///---3--///
+//Сделайте функцию, которая возвращает сумму двух чисел.//
+// function calsSumm(numOne,numTwo){
+//     console.log(`переменная numOne:${numOne}`)
+//     console.log(`перменая numTwo:${numTwo}`)
+//     let numSumm=numOne+numTwo;
+//     console.log(`Сумма:${numSumm}`);
+// }
+// calsSumm(200,10)
+///---4---///
+//Сделайте функцию, которая отнимает от первого числа второе и делит на третье.//
+// function smallCalulator(a,b,c){
+//     console.log(a)
+//     console.log(b)
+//     console.log(c)
+//     let suMma=(a-b)/c;
+//     console.log(suMma);
+// }
+// smallCalulator(100,50,2)//
+            //---5---//
+//Сделайте функцию, которая принимает параметром число от 1 до 7, а возвращает день недели на русском языке.//
+// let a =prompt('напиши-число');
+// switch (a){
+//     case '1':alert("1-это-понидельник");
+//     break;
+//     case '2':alert("2-это-вторник");
+//     break;
+//     case '3':alert("3-это-среда");
+//     break;
+//     case '4':alert("4-это-четверг");
+//     break;
+//     case '5':alert("5-это-пятнитца");
+//     break;
+//     case '6':alert("6-это-суббота");
+//     break;
+//     case '7':alert("7-это-возскрисение");
+//     break;
+//     default:alert("ты чё дурачок")
+// }
